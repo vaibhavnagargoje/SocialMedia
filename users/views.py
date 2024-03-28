fb03d8158161422627af0fc3395c51b9a1767c00
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-03-28 17:27:29 +0530

    update view, urls, user login

--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
+# from django.contrib.auth import
 # from SocialMedia.users.forms import LoginForm   # another way below 
-from .forms import LoginForm
+from .forms import LoginForm,UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 
@@ -12,14 +15,42 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            user = authenticate(request,username =data["username"],password=data["password"])
-            if user is not None:
+            user = authenticate(request,username=data["username"], password=data["password"])
+            if user is not None: 
                 login(request,user)
                 return HttpResponse("User Loged in ")
-                return render(request,"base.html")
+                
             else:
                 return HttpResponse("invalid Login ")
 
     else:
         form = LoginForm()
         return render(request,'users/login.html',{'form':form})
+
+def user_logout(request):
+    logout(request)
+    return render(request,'users/logout.html')
+
+@login_required
+def index(request):
+    return render(request,'users/index.html')
+
+
+def password_change(request):
+
+    return render(request,'users/password_change_form.html')
+
+
+def registor(request):
+    if request.method=='POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user=user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request,'users/registor_done.html')
+
+    else:
+        user_form= UserRegistrationForm()
+    return render(request,'users/registor.html',{'user_form':user_form})
+
